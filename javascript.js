b1747a951bbe4d9c346027e63517eeb0f6e58d2d
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-06-25 17:30:36 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,49 +1,46 @@
 // ESERCIZIO 1
 
-const firstButtonElement = document.querySelector(".insertLocalStorage");
-const inputElement = document.querySelector("input");
-const divDisplay = document.querySelector(".valoriSalvati");
-const secondButtonElement = document.querySelector(".deleteLocalStorage");
-const KEY = "Name";
-const insertElement = () => {
-  const objToString = JSON.stringify(inputElement.value);
-  localStorage.setItem("Name", objToString);
-  console.log(objToString);
-  const valueToDisplay = localStorage.getItem(KEY);
-  const stringToObj = JSON.parse(valueToDisplay);
-  // const visualizeElement = document.createElement("div");
-  // visualizeElement.classList.add("elementoCreato");
-  // visualizeElement.innerText = `${stringToObj}`;
-  // divDisplay.appendChild(visualizeElement);
-  divDisplay.innerText = `${stringToObj}`;
-};
+// const firstButtonElement = document.querySelector(".insertLocalStorage");
+// const inputElement = document.querySelector("input");
+// const divDisplay = document.querySelector(".valoriSalvati");
+// const secondButtonElement = document.querySelector(".deleteLocalStorage");
+// const KEY = "Name";
+// const insertElement = () => {
+//   const objToString = JSON.stringify(inputElement.value);
+//   localStorage.setItem("Name", objToString);
+//   console.log(objToString);
+//   const valueToDisplay = localStorage.getItem(KEY);
+//   const stringToObj = JSON.parse(valueToDisplay);
 
-firstButtonElement.onclick = function (event) {
-  event.preventDefault();
-  insertElement();
-  const visualizeElement = document.querySelector(".elementoCreato");
-  visualizeElement.innerText = "";
-};
+//   divDisplay.innerText = `${stringToObj}`;
+// };
 
-secondButtonElement.onclick = function (event) {
-  event.preventDefault();
-  if (document.querySelector(".elementoCreato")) {
-    localStorage.removeItem(KEY);
-  } else {
-    console.log("non esiste alcun elemento");
-  }
-};
+// firstButtonElement.onclick = function (event) {
+//   event.preventDefault();
+//   insertElement();
+//   const visualizeElement = document.querySelector(".elementoCreato");
+//   visualizeElement.innerText = "";
+// };
+
+// secondButtonElement.onclick = function (event) {
+//   event.preventDefault();
+//   if (document.querySelector(".elementoCreato")) {
+//     localStorage.removeItem(KEY);
+//   } else {
+//     console.log("non esiste alcun elemento");
+//   }
+// };
 
 // ESERCIZIO 2
-// let i = sessionStorage.getItem("count")
-//   ? parseInt(sessionStorage.getItem("count"))
-//   : 0;
+let i = sessionStorage.getItem("count")
+  ? parseInt(sessionStorage.getItem("count"))
+  : 0;
 
-// const myTimer = function () {
-//   i = i + 1;
-//   const divElement = document.querySelector("#timer");
-
-//   divElement.innertext = `${i} + secondi sessione`;
-//   sessionStorage.setItem("count", i);
-// };
-// setInterval(myTimer, 1000);
+const myTimer = function () {
+  i = i + 1;
+  const divElement = document.querySelector("#timer");
+  console.log(divElement);
+  divElement.innertext = `${i} + secondi sessione`;
+  sessionStorage.setItem("count", i);
+};
+setInterval(myTimer, 1000);
